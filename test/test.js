6103d3cc1db66cafa627085363557682c2f16148
arthur-caillaud <arthurcaillaud@gmail.com>
2017-12-05 21:54:26 +0100

    Adding test for alerting module

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const alerting = require('../analytics/alerting');
+const expect = require("chai").expect;
+
+describe("Availibility Alerting Logs", () => {
+    it("preserves existing log messages", () => {
+        const logMessages = [{
+            type: 'up',
+            availibility: 0.85}];
+        const availibilityStats = {
+            statusChanged: true,
+            isAvailable: false,
+            availibility: 0.7
+        };
+        const newLogMessages = alerting(logMessages,availibilityStats);
+
+        //TEST PHASE
+        expect(newLogMessages.length).to.equal(2);
+    });
+});
